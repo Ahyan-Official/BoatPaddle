0c25e99eb775c44bf68f2867fc28e296d24227a5
Azron Ahyan <azronahyan@gmail.com>
2022-06-26 16:24:54 +0500

    Main Activity Spash Screen Register Welcome SubmitData

---
+++ app/src/main/java/com/funpaddler/app/az/SubmitDataActivity.java
@@ -0,0 +1,14 @@
+package com.funpaddler.app.az;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubmitDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_data);
+    }
+}
